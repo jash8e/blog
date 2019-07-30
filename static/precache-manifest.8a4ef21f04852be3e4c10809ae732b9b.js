c6923f5d64ec4e88576bbdcb2ce20c906fad2c4b
jkerush <zzh3258@qq.com>
2019-07-30 15:51:08 +0800

    update static folds

---
+++ static/precache-manifest.8a4ef21f04852be3e4c10809ae732b9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c5df9508894c825cb4c58bdfdb47dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "098d66bb16ca716002d4",
+    "url": "/static/css/main.0b4e3ef1.chunk.css"
+  },
+  {
+    "revision": "6be3e8005be49f003fc0",
+    "url": "/static/js/2.2bfdc5ce.chunk.js"
+  },
+  {
+    "revision": "098d66bb16ca716002d4",
+    "url": "/static/js/main.7c6c8079.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "10504d33148d18911df70783824430ae",
+    "url": "/static/media/cat.10504d33.gif"
+  }
+]);
